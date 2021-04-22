108ba793ea1490988b1c26b078f6a52c9f078001
Jac Phillips <phillipsjacqueline@hotmail.com>
2021-04-22 12:48:53 +0100

    Added application file

--- Sky_Final_Project/application/routes.py
+++ Sky_Final_Project/application/routes.py
@@ -7,13 +7,13 @@ from application import app
 def home():
     return render_template('home.html', title="Home")
 
-<<<<<<< HEAD
+
 @app.route('/film-reviews')
 def film_reviews():
     return render_template('film_reviews.html', title="film-reviews")
-=======
+
+
 @app.route('/new-gods-nezha-reborn')
 def newgodsnezhareborn():
     return render_template('new-gods-nezha-reborn.html', title="New Gods: Nezha Reborn")
 
->>>>>>> 86f68bf59735bf03587a78ad1171ac2a943948f1
