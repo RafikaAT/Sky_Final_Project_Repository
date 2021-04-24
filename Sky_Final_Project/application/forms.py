433dd18a88595e53f5636b88871ccb6cfe283962
Chiara <cjedinan@gmail.com>
2021-04-24 14:22:02 +0100

    home.html modified, forms.py created classes for sign up and login pages, in __init__.py created secret key and imported both forms

---
+++ Sky_Final_Project/application/forms.py
@@ -0,0 +1,22 @@
+from flask_wtf import FlaskForm
+from wtforms import StringField, PasswordField, SubmitField, BooleanField
+from wtforms.validators import DataRequired, Length, Email, EqualTo
+
+
+class SignUpForm(FlaskForm):
+    username = StringField('Username', validators=[DataRequired(), Length(min=3, max=15)])
+    email = StringField('Email Address', validators=[DataRequired(), Email()])
+    password = PasswordField('Password', validators=[DataRequired(), Length(min=6, max=20)])
+    confirm_password = PasswordField('Confirm Password', validators=[DataRequired(), Length(min=6, max=20),
+                            EqualTo('Password')])
+    submit = SubmitField('Create Account')
+
+
+class LoginForm(FlaskForm):
+    username = StringField('Username', validators=[DataRequired(), Length(min=3, max=15)])
+    password = PasswordField('Password', validators=[DataRequired(), Length(min=6, max=20)])
+    remember = BooleanField('Remember Me')
+    submit = SubmitField('Login')
+
+
+
