3e7abed0a01121c4e50e43038b23fc3c9cfb9fde
amberleeshand <amberleeshand@gmail.com>
2021-04-24 15:35:17 +0100

    imported render template, url for

--- Sky_Final_Project/application/__init__.py
+++ Sky_Final_Project/application/__init__.py
@@ -1,7 +1,8 @@
-from flask import Flask
+from flask import Flask, render_template, url_for
 from forms import SignUpForm, LoginForm
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'e3c56yt2bof7'
+
 from application import routes
 
