27f43181ba356170de8a0ab9262afeaa36125728
amberleeshand <amberleeshand@gmail.com>
2021-04-24 17:28:31 +0100

    created login page but doesnt work as of yet

--- Sky_Final_Project/application/routes.py
+++ Sky_Final_Project/application/routes.py
@@ -16,9 +16,15 @@ def sign_up():
         return redirect(url_for('home'))
     return render_template('sign_up.html', title='Sign Up', form=form)
 
-@app.route('/login')
+@app.route("/login", methods=['GET', 'POST'])
 def login():
     form = LoginForm()
+    if form.validate_on_submit():
+        if form.email.data == 'admin@blog.com' and form.password.data == 'password':
+            flash('You have been logged in!', 'success')
+            return redirect(url_for('home'))
+        else:
+            flash('Login Unsuccessful. Please check username and password', 'danger')
     return render_template('login.html', title='Login', form=form)
 
 @app.route('/film-reviews')
