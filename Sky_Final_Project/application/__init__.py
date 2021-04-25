cb547bd38c0bb04b2f9ba43564dbcf5294004c1d
amberleeshand <amberleeshand@gmail.com>
2021-04-25 20:36:49 +0100

    changed sql alchemy base uri

--- Sky_Final_Project/application/__init__.py
+++ Sky_Final_Project/application/__init__.py
@@ -2,7 +2,7 @@ from flask import Flask, render_template, url_for
 from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///site.db'
+app.config['SQLALCHEMY_DATABASE_URI'] = "mysql+pymysql://root:password@localhost/flask_test"
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.config['SECRET_KEY'] = 'e3c56yt2bof7'
 db = SQLAlchemy(app)
