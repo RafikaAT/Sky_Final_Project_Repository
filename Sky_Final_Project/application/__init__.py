6eceefdf04ab321ef487cd8dd52374e2cae427c4
amberleeshand <amberleeshand@gmail.com>
2021-04-26 20:01:01 +0100

    test of setting up data with sqlalchemy

--- Sky_Final_Project/application/__init__.py
+++ Sky_Final_Project/application/__init__.py
@@ -1,10 +1,21 @@
+import app as app
 from flask import Flask, render_template, url_for, flash
 from flask_sqlalchemy import SQLAlchemy
+from application import app
+
+username = 'newuser'
+password = 'password'
+userpass = 'mysql+pymysql://' + username + ':' + password + '@'
+server = '127.0.0.1'
+dbname = '/db'
 
 app = Flask(__name__)
+app.secret_key = 'skyGIT'
+# put them all together as a string that shows SQLAlchemy where the database is
+app.config['SQLALCHEMY_DATABASE_URI'] = userpass + server + dbname
+app.config['SQLALCHEMY_DATABASE_URI'] = "mysql+pymysql://newuser:password@localhost/db"
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
-app.config['SECRET_KEY'] = 'e3c56yt2bof7'
-app.config['SQLALCHEMY_DATABASE_URI'] = "mysql+pymysql://root:password@localhost/movieblog"
+
 
 db = SQLAlchemy(app)
 
