a5712ffa0da9cebee50386c37bd5dc21c051c6e4
amberleeshand <amberleeshand@gmail.com>
2021-04-30 15:15:30 +0100

    added a feature that allows users to access pages they can get into after they log in

--- Sky_Final_Project/application/routes.py
+++ Sky_Final_Project/application/routes.py
@@ -4,7 +4,7 @@ from forms import SignUpForm
 from forms import LoginForm, PostComment
 from models import User
 from application import db, bcrypt
-from flask_login import login_user, current_user, logout_user
+from flask_login import login_user, current_user, logout_user, login_required
 
 
 @app.route('/')
@@ -42,17 +42,21 @@ def login():
         user = User.query.filter_by(email=form.email.data).first()
         if user and bcrypt.check_password_hash(user.password, form.password.data):
             login_user(user, remember=form.remember.data)
-            return redirect(url_for('home'))
+            next_page = request.args.get('next')
+            return redirect(next_page) if next_page else redirect(url_for('home'))
         else:
             flash('Login Unsuccessful. Please check email and password', 'danger')
     return render_template('login.html', title='Login', form=form)
+
 
 @app.route("/logout")
 def logout():
     logout_user()
     return redirect(url_for('home'))
 
+
 @app.route("/account")
+@login_required
 def account():
     return render_template('account.html', title='Account')
 
